769a966684771450395fe1c9b3f6010262dfc70e
Zackafilms <98791568+Zackafilms@users.noreply.github.com>
2022-10-18 09:40:34 +1100

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,9 @@
+<form action="chatbox.php">
+  <p>Username:</p>
+  <input type="text" maxlength="20" required>
+  <p>Email</p>
+  <input type="email" required>
+  <p>Password</p>
+  <input type="passowrd" minlength="6" required>
+  <button type="submit">Signup</button>
+</form>
